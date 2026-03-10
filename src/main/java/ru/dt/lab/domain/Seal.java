7987b58410c1c3cf770a4d1ee426e4892311f63b
egort5515-stack <egort5515@gmail.com>
2026-03-10 20:11:40 +0300

    Seal.java

--- src/main/java/ru/dt/lab/domain/Seal.java
+++ src/main/java/ru/dt/lab/domain/Seal.java
@@ -4,27 +4,88 @@ import java.time.Instant;
 
 public final class Seal {
     // Уникальный номер пломбы. Программа назначает сама.
-    public long id;
+    private final long id;
 
     // К какому образцу относится (id образца).
     // Должен ссылаться на реально существующий Sample.
-
-    public long sampleId;
+    private long sampleId;
 
     // Номер пломбы (например "SEAL-9911"). Нельзя пустое. До 64 символов.
-    public String sealNumber;
+    private String sealNumber;
 
     // Статус пломбы: ACTIVE или BROKEN.
-    public SealStatus status;
+    private SealStatus status;
 
     // Кто поставил пломбу (логин). На ранних этапах можно "SYSTEM".
-    public String ownerUsername;
+    private String ownerUsername;
 
     // Когда поставили. Программа ставит автоматически.
-    public Instant createdAt;
+    private final Instant createdAt;
 
     // Когда изменяли (например, ломали). Программа обновляет автоматически.
-    public Instant updatedAt;
+    private Instant updatedAt;
+
+    public Seal(long id, long sampleId, String sealNumber, SealStatus status, String ownerUsername, Instant createdAt,Instant updatedAt){
+        this.id = id;
+        this.sampleId = sampleId;
+        setSealNumber(sealNumber);
+        setStatus(status);
+        setOwnerUsername(ownerUsername);
+        this.createdAt = createdAt;
+        setUpdatedAt(updatedAt);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getSampleId() {
+        return sampleId;
+    }
+
+    public void setSampleId(long sampleId) {
+        this.sampleId = sampleId;
+    }
+
+    public String getSealNumber() {
+        return sealNumber;
+    }
+
+    public void setSealNumber(String sealNumber) {
+        if(!sealNumber.isEmpty() && sealNumber.length() < 64) {
+            this.sealNumber = sealNumber;
+        } else{
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public SealStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SealStatus status) {
+        this.status = status;
+    }
+
+    public String getOwnerUsername() {
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername) {
+        this.ownerUsername = ownerUsername;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
     @Override
     public boolean equals(Object o) {
